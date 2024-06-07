1ffb21d52252664114cfb66f653c991c87b65d43
Nithin <nithinkatkam504106@gmail.com>
2024-06-07 17:30:11 +0530

    fix array

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -3,7 +3,7 @@ import { BASED } from "./based";
 import Link from "next/link";
 import Connect from "./wallet";
 function Landing() {
-  const themes = ["OG", "1", "2"];
+  const themes = ["OG"];
 
   return (
     <section className="bg-meme bg-[--blue] min-h-auto md:min-h-[95vh] flex flex-col items-start w-screen p-[2.5vw] pb-2 text-white">
@@ -31,9 +31,32 @@ function Landing() {
           The digital world is memetic. Join us for 48 hours of Open Edition
           mints with the most based onchain.
         </p>
+
       </div>
       <div className="flex items-center justify-center w-full">
-       <Connect />
+       {/* <Connect /> */}
+       <Link
+          href="/#art"
+          className="relative cursor-pointer w-[13rem] md:w-[15rem] mt-[3rem] md:mt-[5rem]"
+        >
+          <svg
+            width="100%"
+            height="100%"
+            viewBox="0 0 198 57"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M186 4.00002V7.60578e-06L12 0V4.00001H8V8.00001H4V12H0V45H4V49H8V53L12 53V57L186 57V53H190V49H194V45H198V12H194V8.00001L190 8.00001V4.00001L186 4.00002Z"
+              fill="currentColor"
+            ></path>
+          </svg>
+          <p className="text-[--blue] text-lg absolute top-0 left-0 h-full w-full flex items-center justify-center">
+            get based
+          </p>
+        </Link>
       </div>
       <div className="flex flex-row gap-3 items-center text-white relative pb-[4rem] pt-5 justify-center md:justify-start w-full md:p-0 md:-top-5">
         {themes.map((theme) => (
